93a1ffc88150b246c6b82ec192927925e7aa5c12
zwt <yc_zwt@163.com>
2022-06-16 00:05:45 +0800

    💥feat：add client config

---
+++ webpack.client.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+// 服务端的webpack
+module.exports = {
+	mode: 'development',
+	entry: './client/index.js',
+	output:{
+		filename: 'bundle.js',
+		path: path.resolve(__dirname,'public')
+	},
+	module:{
+		rules:[
+      {
+        test:/\.js$/,
+        // 才能支持import 支持jsx
+        loader:'babel-loader',
+        exclude: /node_modules/,
+        options:{
+          presets:['@babel/preset-react',['@babel/preset-env']]
+        }
+      }
+    ]
+	}
+}
