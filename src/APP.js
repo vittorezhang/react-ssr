349ef5bf36ce9b0468476a88a17a668b57a1e475
zwt <yc_zwt@163.com>
2022-06-19 22:50:45 +0800

    🏰 chore: 路由配置

--- src/APP.js
+++ src/APP.js
@@ -4,9 +4,27 @@ import Index from "./container/index";
 import About from "./container/about";
 
 
-export default (
-	<div>
-		<Route path='/' exact component={Index}></Route>
-		<Route path='/about' exact component={About}></Route>
-	</div>
-)
+// export default (
+// 	<div>
+// 		<Route path='/' exact component={Index}></Route>
+// 		<Route path='/about' exact component={About}></Route>
+// 	</div>
+// )
+
+// 改造成js 的配置，才能获取组件
+
+export default [
+	{
+		path: '/',
+		component: Index,
+		// loadData: Index.loadData,
+		exact: true,
+		key: 'index'
+	},
+	{
+		path: '/about',
+		component: About,
+		exact: true,
+		key: 'about'
+	}
+]
