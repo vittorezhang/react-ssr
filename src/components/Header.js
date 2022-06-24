5f894a4a2605e294f15bca2cd86c60e49fb010bf
zwt <yc_zwt@163.com>
2022-06-24 20:24:55 +0800

    💥feat：添加user栏

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,5 +6,7 @@ export default ()=>{
     <Link to={'/'}>首页</Link>
       <span style={{padding: 10}}>|</span>
     <Link to={'/about'}>关于</Link>
+			<span style={{padding: 10}}>|</span>
+		<Link to={'/user'}>用户信息</Link>
   </div>
 }
