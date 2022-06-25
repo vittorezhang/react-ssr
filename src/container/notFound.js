da0c7a421769dc212b9a9d685fca34a2877637ad
zwt <yc_zwt@163.com>
2022-06-25 09:52:27 +0800

    🌷style：404页面样式调整

--- src/container/notFound.js
+++ src/container/notFound.js
@@ -15,8 +15,10 @@ function Notfound(props){
   // console.log('noutound',props)
   // 渲染了这个组件，给staticContext赋值， statuscode=404
   return <Status code={404}>
-    <h1>大兄弟瞅啥呢</h1>
-    <img id='img-404' src="/404.jpg" alt=""/>
+    <div style={{textAlign:'center'}}>
+      <h1>大兄弟瞅啥呢</h1>
+      <img id='img-404' src="/404.jpg" alt=""/>
+    </div>
   </Status>
 }
 
