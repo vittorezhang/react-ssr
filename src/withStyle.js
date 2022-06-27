4102837a3952adef4dd598efd051898926dac91e
zwt <yc_zwt@163.com>
2022-06-27 10:20:22 +0800

    🏰 chore: withStyle优化

--- src/withStyle.js
+++ src/withStyle.js
@@ -1,4 +1,5 @@
 import React from "react";
+import hoistNonReactStatic from "hoist-non-react-statics";
 // 方案1
 // function withStyle(Comps,styles) {
 // 	return function (props) {
@@ -13,6 +14,21 @@ import React from "react";
 
 // 方案2
 
+// function withStyle(Comps,styles) {
+// 	function NewComps (props) {
+// 		if(props.staticContext){
+// 			props.staticContext.css.push(styles._getCss())
+// 		}
+// 		return <Comps {...props}/>
+// 	}
+//   NewComps.loadData = Comps.loadData
+//   return NewComps
+// }
+
+// export default withStyle
+
+// 方案3，使用react 官方文档进阶指导提供的hoist-non-react-statics
+
 function withStyle(Comps,styles) {
 	function NewComps (props) {
 		if(props.staticContext){
@@ -20,7 +36,7 @@ function withStyle(Comps,styles) {
 		}
 		return <Comps {...props}/>
 	}
-  NewComps.loadData = Comps.loadData
+  hoistNonReactStatic(NewComps,Comps)
   return NewComps
 }
 
